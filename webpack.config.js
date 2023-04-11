b1c78c3f3abaf28035978d3b5878642ac14c9410
Anastasiia Pyzhevska <pyzhiknastya2912@gmail.com>
2023-04-11 17:10:34 +0300

    created webpack modules and dev-server file

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,25 @@
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
+const { devServer } = require('./webpack.dev-server');
+const { modules } = require('./webpack.modules');
 
 module.exports = {
-	mode: 'development',
-	entry: ['@babel/polyfill', './src/index.jsx'],
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: '[name].[hash].js',
-	},
-
-	devServer: {
-		port: 3000,
-	},
-
-	plugins: [new HTMLWebpackPlugin({ template: './public/index.html' }), new CleanWebpackPlugin()],
-	module: {
-		rules: [
-			{
-				test: /\.css$/,
-				use: ['style-loader', 'css-loader'],
-			},
-			{
-				test: /\.(jpg|jpeg|png|svg)$/,
-				use: ['file-loader'],
-			},
-			{
-				test: /\.(js|jsx)$/,
-				exclude: /node_modules/,
-				use: ['babel-loader'],
-			},
-		],
-	},
+  mode: 'development',
+  entry: ['@babel/polyfill', './src/index.jsx'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[hash].js',
+  },
+  plugins: [new HTMLWebpackPlugin({ template: './public/index.html' }), new CleanWebpackPlugin()],
+  devServer,
+  module: modules,
+  resolve: {
+    extensions: ['.ts', '.js', '.jsx'],
+    modules: ['src', 'node_modules'],
+    alias: {
+      components: path.resolve(__dirname, 'components/'),
+      common: path.resolve(__dirname, 'common/'),
+    },
+  },
 };
